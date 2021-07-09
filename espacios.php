3074cfe0cf68eec8c763097b8ac5febd3c170659
2021 DSM33 FATIMA MONSERRAT GARCIA DAMIAN <al222010694@gmail.com>
2021-07-09 01:00:30 -0500

    Creacion del formulario espacios segunda parte

--- espacios.php
+++ espacios.php
@@ -15,7 +15,7 @@
                 Espacios
             </div>
             <div class="card-body">
-                <form action="espacios_guarda.php" method="POST">
+                <form action="espacios_guarda.php" method="POST" class="needs-validation" novalidate>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="mb-3"> 
@@ -33,7 +33,7 @@
                                 </div>
                             </div>
                             <div class="mb-3">
-                                <label for="metros" class="form-label">Metros</label>
+                                <label for="metros" class="form-label">Metros (cuadrados)</label>
                                 <input type="text" name="metros" required class="form-control form-control-sm" id="metros">
                                 <div class="invalid-feedback">
                                     Ingresa los metros
@@ -58,14 +58,14 @@
                                 </div>
                             </div>
                             <div class="mb-3">
-                                <label class="form-label" for="estado">Estado</label>
-                                <select class="form-select" id="estado" required class="form-control form-control-sm">
-                                    <option selected>...</option>
+                                <label class="form-label" for="estado-del-espacio">Estado del espacio</label>
+                                <select class="form-select" id="estado-del-espacio" required class="form-control form-control-sm">
+                                    <option selected value="">...</option>
                                     <option value="1">Disponible</option>
                                     <option value="2">Prestado</option>
                                 </select>
                                 <div class="invalid-feedback">
-                                    Selecciona el estado
+                                    Selecciona el estado del espacio
                                 </div>
                             </div>
                             <div class="row">
@@ -89,14 +89,79 @@
                                 </div>
                             </div>
                         </div>
-                        
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="mb-3">
+                                    <label class="form-label" for="estado_id">Estado</label>
+                                    <select class="form-select" id="estado_id" required class="form-control form-control-sm">
+                                        <option selected value="">Estado...</option>
+                                    </select>
+                                    <div class="invalid-feedback">
+                                        Selecciona un estado
+                                    </div>
+                                </div> 
+                                <div class="mb-3">
+                                    <label class="form-label" for="municipio_id">Municipio</label>
+                                    <select class="form-select" id="municipio_id" required class="form-control form-control-sm">
+                                        <option selected value="">Municipio...</option>
+                                    </select>
+                                    <div class="invalid-feedback">
+                                        Selecciona un municipio
+                                    </div>
+                                </div> 
+                                <div class="row">
+                                    <div class="mb-3">
+                                        <label for="calle" class="form-label">Calle</label>
+                                        <input type="text" name="calle" required class="form-control form-control-sm" id="calle">
+                                        <div class="invalid-feedback">
+                                            Ingresa tu calle
+                                        </div>
+                                    </div> 
+                                    <div class="mb-3">
+                                        <label for="colonia" class="form-label">Colonia</label>
+                                        <input type="text" name="colonia" required class="form-control form-control-sm" id="colonia">
+                                        <div class="invalid-feedback">
+                                            Ingresa tu calonia
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                            <div class="col-sm-4">
+                                <div class="mb-3">
+                                    <label for="numero_exterior" class="form-label">Número exterior</label>
+                                    <input type="text" name="numero_exterior" required class="form-control form-control-sm" id="numero_exterior">
+                                    <div class="invalid-feedback">
+                                        Escribe tu número exterior
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="mb-3">
+                                    <label for="numero_interior" class="form-label">Numero interior</label>
+                                    <input type="text" name="numero_interior" class="form-control form-control-sm" id="numero_interior" aria-describedby="numero_interiorHelp">
+                                    <div id="numero_interiorHelp" class="form-text">Opcional</div>
+                                    <div class="valid-feedback">
+                                         Opcional
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="mb-3">
+                                    <label for="codigo_postal" class="form-label">Cóigo postal</label>
+                                    <input type="text" name="codigo_postal" required class="form-control form-control-sm" id="codigo_postal">
+                                    <div class="invalid-feedback">
+                                        Escribe tu código postal
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-sm btn-primary">Enviar</button>
                     </div>
                 </form>    
             </div>   
         </div>
     </div>
-    
-
-    
+    <script src="js/validacion_bootstrap.js"></script>  
 </body>
 </html>
